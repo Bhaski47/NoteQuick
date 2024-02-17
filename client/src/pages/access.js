7c1d9006b906214f3ba59a73a5c896575ba0f726
Bhaski47 <bhaskarg2003@gmail.com>
2024-02-17 15:30:10 +0530

    Added Loader

--- client/src/pages/access.js
+++ client/src/pages/access.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import styles from "../styles/access.module.scss";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
+import Loader from "../utils/Loader";
 
 function Login() {
   const nav = useNavigate();
@@ -9,47 +11,52 @@ function Login() {
   const [pass, setPass] = useState("");
   const [email, setEmail] = useState("");
   const [isLogin, setLogin] = useState(true);
-  const [error, setError] = useState(false);
-  const [resError, setReserror] = useState("");
+  const [isLoad, setLoad] = useState(false);
   useEffect(() => {
-   isLogin ? document.title = "Register | Todo" : document.title = "Login | Todo" ;
+    isLogin
+      ? (document.title = "Register | Todo")
+      : (document.title = "Login | Todo");
   }, [isLogin]);
   const handleLogin = async (e) => {
     e.preventDefault();
+    setLoad(true)
     try {
-      const token = await axios.post("https://localhost:3000/auth/log", {
-        pass: pass,
-        email: email,
-      });
+      const token = await axios.post(
+        "https://backendtodo-s91r.onrender.com/auth/log",
+        {
+          pass: pass,
+          email: email,
+        }
+      );
       const tok = token.data.data;
       localStorage.setItem("myTok", tok);
-      localStorage.setItem("email",email);
-      setReserror("Logging In");
-      setLogin(false);
+      localStorage.setItem("email", email);
+      setLoad(false)
+      successToast("Logging In");
       nav("/todo");
     } catch (err) {
-      setReserror(err.response.data.message);
-      setError(true);
+      setLoad(false)
+      errorToast(err.response.data.message);
     }
-  };
-  const errorDetector = () => {
-    setError(false);
-    setReserror("");
   };
   const handleSign = async (e) => {
     e.preventDefault();
-    errorDetector();
+    setLoad(true)
     const data = {
       name: username,
       email: email,
       pass: pass,
     };
     try {
-      const token = await axios.post("https://localhost:3000/auth/create", data);
-      setReserror(token.data.message);
+      const token = await axios.post(
+        "https://backendtodo-s91r.onrender.com/auth/create",
+        data
+      );
+      successToast(token.data.message);
+      setLoad(false)
     } catch (err) {
-      setReserror(err.response.data.message);
-      setError(true);
+      setLoad(false)
+      errorToast(err.response.data.message);
     }
   };
 
@@ -59,7 +66,6 @@ function Login() {
         className={styles.accessChange}
         onClick={() => {
           setLogin((prev) => !prev);
-          errorDetector();
         }}
       >
         Don't Have an Account??
@@ -72,71 +78,81 @@ function Login() {
         className={styles.accessChange}
         onClick={() => {
           setLogin((prev) => !prev);
-          errorDetector();
         }}
       >
         Already Have an Account??
       </p>
     );
   };
+
+  const errorToast = (value) => toast.error(value);
+  const successToast = (value) => toast.success(value);
+
   return (
     <div className={styles.centerBox}>
+      <Toaster />
       <div className={styles.content}>
-        {isLogin ? (
-          <h2 className={styles.header}>Register</h2>
-        ) : (
-          <h2 className={styles.header}>Login</h2>
+        {isLoad && (
+          <div className={styles.loader}>
+            <Loader />
+          </div>
         )}
-        <form
-          onSubmit={isLogin ? handleSign : handleLogin}
-          className={styles.form}
-        >
-          {isLogin && (
-            <div className={styles.changeUser}>
-              <label className={styles.label} htmlFor="Username">
-                Username
-              </label>
-              <br />
-              <input
-                className={styles.input}
-                onChange={(e) => setUser(e.target.value)}
-                type="text"
-                required
-              />
-            </div>
-          )}
-          <label className={styles.label} htmlFor="Email">
-            Email
-          </label>
-          <br />
-          <input
-            className={styles.input}
-            type="email"
-            required
-            autoComplete="false"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <br />
-          <label className={styles.label} htmlFor="Password">
-            Password
-          </label>
-          <br />
-          <input
-            className={styles.input}
-            type="password"
-            required
-            autoComplete="false"
-            onChange={(e) => setPass(e.target.value)}
-          />
-          <br />
-          {resError && !error && <p>{resError}</p>}
-          {error && <p>{resError}</p>}
-          <button type="submit" className={styles.btn} autoComplete="false">
-            Submit
-          </button>
-        </form>
-        {isLogin ? existAcc() : noAcc()}
-        
+
+        {!isLoad &&
+          (isLogin ? (
+            <h2 className={styles.header}>Register</h2>
+          ) : (
+            <h2 className={styles.header}>Login</h2>
+          ))}
+        {!isLoad && (
+          <form
+            onSubmit={isLogin ? handleSign : handleLogin}
+            className={styles.form}
+          >
+            {isLogin && (
+              <div className={styles.changeUser}>
+                <label className={styles.label} htmlFor="Username">
+                  Username
+                </label>
+                <br />
+                <input
+                  className={styles.input}
+                  onChange={(e) => setUser(e.target.value)}
+                  type="text"
+                  required
+                />
+              </div>
+            )}
+            <label className={styles.label} htmlFor="Email">
+              Email
+            </label>
+            <br />
+            <input
+              className={styles.input}
+              type="email"
+              required
+              autoComplete="off"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <br />
+            <label className={styles.label} htmlFor="Password">
+              Password
+            </label>
+            <br />
+            <input
+              className={styles.input}
+              type="password"
+              required
+              autoComplete="off"
+              onChange={(e) => setPass(e.target.value)}
+            />
+            <br />
+            <button type="submit" className={styles.btn} autoComplete="off">
+              Submit
+            </button>
+          </form>
+        )}
+        {!isLoad && (isLogin ? existAcc() : noAcc())}
       </div>
     </div>
   );
