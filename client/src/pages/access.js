60226c936263438df13b980bc095f038e72b5177
Bhaski47 <bhaskarg2003@gmail.com>
2024-03-31 22:13:59 +0530

    Update

--- client/src/pages/access.js
+++ client/src/pages/access.js
@@ -23,7 +23,7 @@ function Login() {
     setLoad(true)
     try {
       const token = await axios.post(
-        `https://backendtodo-s91r.onrender.com/auth/log`,
+        `${process.env.PUBLIC_REACT_BACKEND_URL}/auth/log`,
         {
           pass: pass,
           email: email,
@@ -50,7 +50,7 @@ function Login() {
     };
     try {
       const token = await axios.post(
-        `https://backendtodo-s91r.onrender.com/auth/create`,
+        `${process.env.PUBLIC_REACT_BACKEND_URL}/auth/create`,
         data
       );
       successToast(token.data.message);
